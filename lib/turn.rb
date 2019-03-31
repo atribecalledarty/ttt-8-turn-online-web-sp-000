695b6849be573b0ca1ab0cde78bf1e632c7a40ff
Arthur Song <arth3rs0ng@gmail.com>
2019-03-31 22:39:50 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,7 +12,7 @@ def input_to_index (input)
 end
 
 def position_taken? (board, index)
-  if (board[index] == " ") || (board[index] == "") || (board[index] = nil)
+  if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
     false
   elsif (board[index] == "X") || (board[index] == "O")
     true
